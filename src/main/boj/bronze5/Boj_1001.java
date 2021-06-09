0e73b641d0b1d4739aaa0d8a6259789b6c43012e
jinsuSang <twtz0309@naver.com>
2021-06-09 23:08:59 +0900

    solved

---
+++ src/main/boj/bronze5/Boj_1001.java
@@ -0,0 +1,14 @@
+package main.boj.bronze5;
+
+import java.util.Scanner;
+
+public class Boj_1001 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte a = (byte) sc.nextInt();
+        byte b = (byte) sc.nextInt();
+
+        System.out.println(a - b);
+    }
+}
