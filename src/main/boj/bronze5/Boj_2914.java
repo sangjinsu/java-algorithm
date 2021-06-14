ea99babfa9da3dec442b2f2d5b51961f4b11dc14
jinsuSang <twtz0309@naver.com>
2021-06-14 22:40:26 +0900

    solved

---
+++ src/main/boj/bronze5/Boj_2914.java
@@ -0,0 +1,16 @@
+package main.boj.bronze5;
+
+import java.util.Scanner;
+
+public class Boj_2914 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        byte songs = sc.nextByte();
+        byte average = sc.nextByte();
+
+        short melodies = (short) (songs * (average - 1) + 1);
+
+        System.out.println(melodies);
+    }
+}
