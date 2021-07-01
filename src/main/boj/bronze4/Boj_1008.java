ca6d76dc3ce87abf828c6dedeff62834bdc0c463
jinsuSang <twtz0309@naver.com>
2021-07-01 21:44:10 +0900

    solved

---
+++ src/main/boj/bronze4/Boj_1008.java
@@ -0,0 +1,31 @@
+package main.boj.bronze4;
+
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Boj_1008 {
+    public static void main(String[] args) throws IOException {
+        /*
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+
+        System.out.print(a/b);
+        */
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        Double a = Double.parseDouble(st.nextToken());
+        Double b = Double.parseDouble(st.nextToken());
+
+        Double result = a/b;
+        bw.write(String.valueOf(result));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+}
