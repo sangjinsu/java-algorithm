468a7d134ca2c84abd64723981a28cf4c1b3b6e1
jinsuSang <twtz0309@naver.com>
2021-07-02 12:11:46 +0900

    solved

---
+++ src/main/boj/bronze3/Boj11022.java
@@ -0,0 +1,34 @@
+package main.boj.bronze3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj11022 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            int t = Integer.parseInt(br.readLine());
+            for (int i = 0; i < t; i++) {
+                st = new StringTokenizer(br.readLine());
+                int a = Integer.parseInt(st.nextToken());
+                int b = Integer.parseInt(st.nextToken());
+
+                sb.append("Case #")
+                        .append(i + 1)
+                        .append(": ").append(a).append(" + ").append(b)
+                        .append(" = ").append(a + b).append('\n');
+            }
+
+            System.out.print(sb);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
